a3ef5bae69db314fde19f1c48d52c1615c49effd
Charukajalanidu-ofc <154059188+Charukajalanidu-ofc@users.noreply.github.com>
2024-12-22 16:40:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: 'IZUMI=rrRmVTrS#jyJx0L7IojIX-n0ZoA_y48v8djZqJCVItnTNGLj8Dwg',
+SESSION_ID: 'rrRmVTrS#jyJx0L7IojIX-n0ZoA_y48v8djZqJCVItnTNGLj8Dwg',
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'pat_11BEXMDNA0vAVKYQXfREPc_6fNxfCUcfAwJAbNaqy5lrSPwhzGnGw1y1YmNsKVAFsrRASWDASYnndxegvS' : process.env.GITHUB_AUTH_TOKEN,
 GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'charukajalanidu-ofc' : process.env.GITHUB_USER_NAME,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94775228949' : process.env.OWNER_NUMBER
